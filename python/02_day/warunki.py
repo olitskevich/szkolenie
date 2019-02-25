1875c0c46cc857964294d0f371ae0607c68a44d9
Olga Tuleja <olgalickewicz@uber.com>
2019-02-25 20:42:46 +0100

    wszystkie zmienne

---
+++ python/02_day/warunki.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+zmienna1 = 1
+zmienna2 = 'text'
+zmienna3 = True
+zmienna4 = []
+zmienna5 = 3.14
+zmienna6 = None
+
+print(zmienna1)
+print(zmienna2)
+print(zmienna3)
+print(zmienna4)
+print(zmienna5)
+print(zmienna6)
+
