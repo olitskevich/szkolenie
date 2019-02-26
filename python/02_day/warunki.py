ad85d994ee835ff9539cd747eb5ea5fc61c52238
Olga Tuleja <olgalickewicz@uber.com>
2019-02-26 22:07:53 +0100

    Changes

--- python/02_day/warunki.py
+++ python/02_day/warunki.py
@@ -14,3 +14,17 @@ print(zmienna4)
 print(zmienna5)
 print(zmienna6)
 
+#PEP8
+if zmienna1 == 3:
+    # esli WARUNEK jest spelniony
+    print (3)
+elif zmienna1 == 4:    
+    # esli INNY_WARUNEK jest spelniony
+    print(4)
+elif zmienna1 == 'blabla':
+    print('blabla')
+elif zmienna1 == 9:
+    print('1')        
+else:
+    # instrukcja jesli zaden z powyzszych nie jest spelniony
+    print ('nie ma takiej zmiennej')
