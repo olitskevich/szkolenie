6881709a63f5c2142c2c28f32e5475a44089b67a
Olga Tuleja <olgalickewicz@uber.com>
2019-02-27 20:29:43 +0100

    petle

---
+++ python/03_dzien/petle.py
@@ -0,0 +1,14 @@
+
+#lista = [5, 3.14, "Olga", True, [0,1,2,3]]
+#lista = ["o", "l", "g", "a"]
+
+#print("Przed pentla")
+
+#for znak in lista: #range(11)
+#   print(znak)
+
+#print("Po petli")
+
+
+for liczba in range(21):
+    print (str(liczba) + ":" + str(liczba**2)+ str(liczba**0.5))
