2cd48e9db268e2f675a3e4d0d47c7cedbfff968c
Olga Tuleja <olgalickewicz@uber.com>
2019-03-02 10:19:02 +0100

    uczestniki

---
+++ python/04_day/zadanie_1.py
@@ -0,0 +1,11 @@
+uczestnik = "Tomek"
+dziennik =["Olga", "Tomek", "Piotr"]
+
+for pozycja, uczen in enumerate(dziennik):
+    pass #print(pozycja, ":", uczen)
+
+    if uczen == uczestnik:
+        print(pozycja, ":", uczen)
+
+#if uczestnik in dziennik:
+#    print("Znalazlam")    
