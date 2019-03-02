11359802e4d8394c4da0502c71cd03ee92eb417a
Olga Tuleja <olgalickewicz@uber.com>
2019-03-02 12:29:39 +0100

    zadanie poprawki

--- python/04_day/zadanie_1.py
+++ python/04_day/zadanie_1.py
@@ -2,10 +2,10 @@ uczestnik = "Tomek"
 dziennik =["Olga", "Tomek", "Piotr"]
 
 for pozycja, uczen in enumerate(dziennik):
-    pass #print(pozycja, ":", uczen)
+    #pass print(pozycja, ":", uczen)
 
     if uczen == uczestnik:
         print(pozycja, ":", uczen)
 
 #if uczestnik in dziennik:
-#    print("Znalazlam")    
+#    print("Znalazlam") 
