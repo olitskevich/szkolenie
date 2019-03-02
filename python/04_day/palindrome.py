2d80db82c854cac28a64700460dee155e2bb8ff0
Olga Tuleja <olgalickewicz@uber.com>
2019-03-02 12:29:02 +0100

    palindrome

---
+++ python/04_day/palindrome.py
@@ -0,0 +1,16 @@
+#Sprawdz czy zadany tekst jest palindromem
+
+text = "piotrrtoiX"
+if_palindrome = True 
+for position, letter in enumerate(text):
+    if text[position] == text[-position - 1]:
+        continue
+    else:
+        if_palindrome = False  
+        break 
+
+if if_palindrome:
+    print("It is a Polindrome")
+else:
+    print("It is NOT a Polindrome")
+
