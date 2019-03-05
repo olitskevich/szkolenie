8fc1e4a372720e148dd89d88329537f5f31110fe
Olga Tuleja <olgalickewicz@uber.com>
2019-03-05 21:36:42 +0100

    inne typy

---
+++ python/03_dzien/inne_typy.py
@@ -0,0 +1,22 @@
+#tuple
+krotka = (1, 2, 3, 4, 5)   
+print(krotka[0])
+
+#dict
+slownik = {   
+    "haslo": "definicja",
+    "amendment": "poprawka",
+    "pi": 3.14,
+    (3,14): "pi"
+}
+
+slownik["haslo"]=> "definicja"
+slownik[(3, 14)]=> "pi"
+
+#set
+#elementy w zbiorze sie nie powtarzaja
+#wynik bedzie unikatowe elementy ->> 1,2,3
+zbior = {1, 2, 3, 2,3}
+
+#immutable
+1 = 2 
